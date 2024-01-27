30373fa741dde4ab4b09d5cb283f4ce05768e6be
mbaula <markbaula.1@gmail.com>
2024-01-26 22:26:39 -0500

    project item done

--- COMP229-PortfolioWebsite/src/components/ProjectItem.jsx
+++ COMP229-PortfolioWebsite/src/components/ProjectItem.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
-import { Box, Image, Text, Heading, HStack } from '@chakra-ui/react';
-import { Link } from '@chakra-ui/react';
+import { Box, Image, Text, Heading, HStack, Link, Badge, useBreakpointValue, VStack } from '@chakra-ui/react';
 
-const ProjectItem = ({ title, description, imageUrl, externalLink, reverse }) => {
-    const itemLayout = reverse ? "row-reverse" : "row";
+const ProjectItem = ({ title, description, imageUrl, externalLink, reverse, badges, textColor }) => {
+    const StackComponent = useBreakpointValue({ base: VStack, md: HStack });
+    const itemLayout = useBreakpointValue({ base: 'column', md: reverse ? 'row-reverse' : 'row' });
 
     return (
-        <HStack spacing={8} align="center" flexDirection={itemLayout} py={5}>
-        <Link href={externalLink} isExternal> 
-        <Box
-            width="350px"
-            height="350px"
-            backgroundImage={`url(${imageUrl})`}
-            backgroundSize="cover"
-            backgroundPosition="center"
-            transition="0.3s"
-            _hover={{ transform: "scale(1.05)" }}
-        />
-        </Link>
-        <Box>
-            <Heading as="h3" size="lg">{title}</Heading>
-            <Text fontSize="md">{description}</Text>
-        </Box>
-        </HStack>
+        <StackComponent spacing={8} align="center" flexDirection={itemLayout} py={5}>
+            <Link href={externalLink} isExternal>
+            <Box
+                maxWidth="100%" 
+                width={{ base: '300px', md: '500px' }}
+                height={{ base: '300px', md: '350px' }}
+                backgroundImage={`url(${imageUrl})`}
+                backgroundSize="cover"
+                backgroundPosition="center"
+                transition="0.3s"
+                _hover={{ transform: "scale(1.05)" }}
+            />
+            </Link>
+            <Box maxWidth="100%" w="100%">
+                <Heading as="h3" size={{ base: 'md', md: 'lg' }} py={2} _hover={{color: '#E94560', transition: "0.5s"}}>{title}</Heading>
+                <HStack spacing={2} wrap="wrap">
+                    {badges.map((badge) => (
+                        <Badge key={badge.text} colorScheme={badge.colorScheme}>
+                            {badge.text}
+                        </Badge>
+                    ))}
+                </HStack>
+                <Text py={6} fontSize={{ base: 's', md: 'lg' }} width="100%" wordBreak="break-word">
+                    {description}
+                </Text>
+            </Box>
+
+        </StackComponent>
     );
 };
 
